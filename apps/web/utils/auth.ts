d190cfb6e6033a1a1db152f898da43d425d94b57
pegasus4me <safoantouil@outlook.fr>
2025-11-27 23:06:38 +0100

    addred drizzle schema

--- apps/web/utils/auth.ts
+++ apps/web/utils/auth.ts
@@ -2,16 +2,18 @@ import { betterAuth } from "better-auth";
 import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import * as schema from "../auth-schema";
 import env from 'dotenv';
 
 env.config();
 
 const sql = neon(process.env.DATABASE_URL || "postgresql://postgres:Rayan_2008@db.nwqzqzzgrbdergqehnso.supabase.co:6543/postgres");
-const db = drizzle(sql);
+const db = drizzle(sql, { schema });
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
-        provider: "pg"
+        provider: "pg",
+        schema
     }),
     emailAndPassword: {
         enabled: true,
