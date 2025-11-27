680042b20618ef3627fa6e7d15cf83d6682ac018
pegasus4me <safoantouil@outlook.fr>
2025-11-27 17:53:56 +0100

    auth fix

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
-const {useSession, signIn, signOut} = createAuthClient({
+const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
 });
 
