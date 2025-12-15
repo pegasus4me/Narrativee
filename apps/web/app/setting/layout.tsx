dfa32bf98c50c3dd6d6e62e1099677d99e5d72bc
pegasus4me <safoantouil@outlook.fr>
2025-12-15 17:16:03 +0100

    rm ann

--- apps/web/app/setting/layout.tsx
+++ apps/web/app/setting/layout.tsx
@@ -1,5 +1,4 @@
 import { SideBar } from "../components/sideBar";
-import AnnouncemebtBar from "../components/announcementBar";
 
 export default function SettingsLayout({
     children,
@@ -8,7 +7,6 @@ export default function SettingsLayout({
 }) {
     return (
         <>
-            <AnnouncemebtBar />
             <section className="h-screen flex flex-row overflow-hidden bg-white">
                 <SideBar />
                 <main className="flex-1 h-full relative overflow-y-auto bg-gray-50">
