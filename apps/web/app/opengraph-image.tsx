2031e4aee941d5fd68112767dd024f8b56e40521
pegasus4me <safoantouil@outlook.fr>
2026-01-02 19:39:01 +0100

    favcon

--- apps/web/app/opengraph-image.tsx
+++ apps/web/app/opengraph-image.tsx
@@ -3,7 +3,7 @@ import { ImageResponse } from 'next/og'
 export const runtime = 'edge'
 
 // Image metadata
-export const alt = 'Narrativee - Turn Excel data into narrative reports'
+export const alt = 'Narrativee -  '
 export const size = {
     width: 1200,
     height: 630,
