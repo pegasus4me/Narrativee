3799c0913e96b08e92c4276bc9509d3b3dc6c8d1
pegasus4me <safoantouil@outlook.fr>
2026-03-17 13:12:40 +0100

    page structure

--- apps/web/app/workspace/post-queue/page.tsx
+++ apps/web/app/workspace/post-queue/page.tsx
@@ -11,17 +11,18 @@ export default function PostQueuePage() {
     return (
         <div className="h-full p-8 overflow-y-auto relative ">
             <div className="max-w-7xl mx-auto h-full flex flex-col gap-6">
-                <div>
-                    <h1 className="text-3xl text-gray-100">Post Queue</h1>
-                    <p className="text-gray-400">Manage your scheduled content and draft ideas.</p>
-                    
-                </div>
+                <div className="flex justify-between p-4">
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-3xl text-gray-100">Post Queue</h1>
+                        <p className="text-gray-400">Manage your scheduled content and draft ideas.</p>
+                    </div>
+                    {/* Creation Tools */}
+                    <div className="">
+                        <ActionToolbar
+                            onGenerateNotes={() => setIsNoteModalOpen(true)}
+                        />
+                    </div>
 
-                {/* Creation Tools */}
-                <div className="p-4 rounded-xl">
-                    <ActionToolbar
-                        onGenerateNotes={() => setIsNoteModalOpen(true)}
-                    />
                 </div>
 
                 <div className="flex-1 min-h-[800px]">
