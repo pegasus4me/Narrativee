634e9377a0e4c08d930e651207b256c164c71aa0
pegasus4me <safoantouil@outlook.fr>
2026-05-20 20:56:01 +0200

    stripe

--- apps/backend/src/config/stripe.ts
+++ apps/backend/src/config/stripe.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 console.log(process.env.STRIPE_SECRET_KEY);
 
-const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || 'sk_test_dummy_key_narrativee';
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || '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';
 
 export const stripe = new Stripe(STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16' as any,
